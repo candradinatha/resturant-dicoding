af85120e90f667d2473949bb3d90d4ee30df55d6
Bagus Candradinatha <putubagus32@gmail.com>
2023-01-19 20:34:28 +0800

    separate layout from controller

---
+++ lib/widgets/restaurant/dialog_post_a_review.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:restaurant/data/controller/restaurant_detail_controller.dart';
+
+import '../../constants/app_sizes.dart';
+import '../../styles.dart';
+import '../common/custom_text_field.dart';
+import '../common/primary_button.dart';
+
+class DialogPostAReview extends StatelessWidget {
+  DialogPostAReview({super.key});
+
+  final RestaurantDetailController controller = Get.find();
+  final reviewerCtrl = TextEditingController();
+  final reviewCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(Sizes.p24),
+          color: colorBgDarkAccent,
+        ),
+        padding: const EdgeInsets.all(Sizes.p24),
+        clipBehavior: Clip.hardEdge,
+        child: Form(
+          key: controller.formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Create a review",
+                style: Get.theme.textTheme.headline6,
+              ),
+              AppGaps.h24,
+              CustomTextForm(
+                controller: reviewerCtrl,
+                hint: "Reviewer Name",
+                isRequired: true,
+              ),
+              AppGaps.h16,
+              CustomTextForm(
+                controller: reviewCtrl,
+                hint: "Review",
+                isRequired: true,
+                textInputType: TextInputType.multiline,
+              ),
+              AppGaps.h24,
+              PrimaryButton(
+                title: "Submit",
+                onTap: () {
+                  if (controller.formKey.currentState?.validate() == true) {
+                    controller.postAReview(
+                      reviewer: reviewerCtrl.value.text,
+                      review: reviewCtrl.value.text,
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
